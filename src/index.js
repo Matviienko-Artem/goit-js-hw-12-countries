527a3b326844666d1730358bde9d04202c714c7d
Matviienko Artem <k2141092mav@gmail.com>
2021-01-24 12:08:47 +0200

    Cделал 12 ДЗ

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import './styles.css';
+import countryCardTpl from './templates/country-card.hbs';
+import countryListTpl from './templates/country-list.hbs';
+import fetchCountryByName from './js/fetchCountries';
+import getRefs from './js/get-refs.js';
+
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/Material.css';
+import { alert, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+
+defaultModules.set(PNotifyMobile, {});
+
+const refs = getRefs();
+
+refs.searchFormInput.addEventListener('input', _.debounce(searchCountry, 500));
+
+function searchCountry(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  const searchQuery = refs.searchFormInput.value;
+
+  fetchCountryByName(searchQuery)
+    .then(renderCountryList)
+    .catch(error => console.log(error));
+}
+
+function renderCountryList(countryies) {
+  refs.cardForCountry.innerHTML = '';
+
+  if (countryies.length === 1) {
+    const oneCountry = countryCardTpl(countryies);
+    refs.cardForCountry.innerHTML = oneCountry;
+  } else if (countryies.length < 10) {
+    const manyCountries = countryListTpl(countryies);
+    refs.cardForCountry.innerHTML = manyCountries;
+  } else {
+    alert({
+      title: `Найдено ${countryies.length} стран`,
+      text: 'Пожалуйста введите более точную информацию',
+      shadow: true,
+      delay: 3000,
+      width: '500px',
+      styling: 'material',
+    });
+  }
+}
---
+++ src/js/get-refs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+  return {
+    cardForCountry: document.querySelector('.js-card-container'),
+    searchFormInput: document.querySelector('.search-form-input'),
+  };
+}
